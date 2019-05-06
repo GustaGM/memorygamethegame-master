92e4c667ae10842357825cf6983e512208aed8af
GustaGM <gustavomartinsdelima@hotmail.com>
2019-05-06 16:42:55 -0300

    a

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
-let cards = [];            
+//lista de todos os cards da classe "memory-card" colocadas em uma variável tipo let
+let cards = document.querySelectorAll('.memory-card');            
 
-function cardNConfirm(event){
-    console.log(event.keycode);
+function flipCard(){
+    //toggle = se a classe estiver lá, remova-a. Se não estiver, coloque-a
+    this.classList.toggle('flip');
 } 
+
+//para cada um dos cards na lista, criamos um "event listener", que cada vez que o evento 'click' for chamado ele irá executar uma função
+cards.forEach(card => card.addEventListener('click',flipCard))
